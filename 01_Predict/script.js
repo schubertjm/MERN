2b1693125f36f77ebf326a53c825cf16daa65312
Jason Schubert <jasonschubert17@gmail.com>
2022-05-03 16:58:04 -0400

    Update script.js

--- 01_Predict/script.js
+++ 01_Predict/script.js
@@ -9,3 +9,38 @@ console.log(otherRandomCar) //Mercedes
 
 //problem 2
 
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(employee.name); //Elon
+console.log(otherName); //Elon
+
+//problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+
+
+
+
