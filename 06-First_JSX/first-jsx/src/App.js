0af7acb11ed4e51a0604ffba5d49df05764cbcb8
Jason Schubert <jasonschubert17@gmail.com>
2022-05-04 19:43:19 -0400

    Assignment 6

---
+++ 06-First_JSX/first-jsx/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Feed Kids</li>
+        <li>Put the Kids to Bed</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
