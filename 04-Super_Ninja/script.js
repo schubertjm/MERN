53516bf5679f635e0ea8de82f6413e2521a771a1
Jason Schubert <jasonschubert17@gmail.com>
2022-05-03 20:17:45 -0400

    Create script.js

---
+++ 04-Super_Ninja/script.js
@@ -0,0 +1,31 @@
+class Ninja {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName = () => console.log(`Ninja name is ${ this.name }`);
+    showStats = () => console.log(`Ninja name is ${ this.name } with a strength of ${ this.strength }, speed of ${ this.speed }, and health of ${ this.health }.`);
+    drinkSake = () => this.health += 10;
+}
+class Sensei extends Ninja {
+    constructor(name, health) {
+        super(name, health);
+        this.wisdom = 10; 
+    }
+    speakWisdom = () => this.health += 20;
+}
+
+const n1 = new Ninja("Jason", 10);
+
+n1.sayName();
+n1.showStats();
+n1.drinkSake();
+n1.showStats();
+
+const n2 = new Sensei("Michael", 20);
+
+n2.showStats();
+n2.speakWisdom();
+n2.showStats();
